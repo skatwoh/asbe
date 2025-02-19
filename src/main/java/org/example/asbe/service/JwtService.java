8246a0eca8c9962d9db0df33ed825b564d478fa0
Thaothantien <ntthao0809@gmail.com>
2025-02-19 17:41:41 +0700

    create book api

--- src/main/java/org/example/asbe/service/JwtService.java
+++ src/main/java/org/example/asbe/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -19,8 +20,11 @@ import java.util.function.Function;
 @Component
 public class JwtService {
 
-    // Replace this with a secure key in a real application, ideally fetched from environment variables
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    @Value("${app.auth.tokenSecret}")
+    private String SECRET;
+
+    @Value("${app.auth.tokenExpirationMsec}")
+    private long jwtExpiration;
 
     // Generate token with given user name
     public String generateToken(String userName, List<String> roles) {
@@ -37,7 +41,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // Token valid for 30 minutes
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration)) // Token valid for 30 minutes
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
