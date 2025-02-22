a65b37a326619eb2e01b6da7a27518cb1ac5379a
Thaothantien <ntthao0809@gmail.com>
2025-02-22 19:12:21 +0700

    fix bug

--- src/main/java/org/example/asbe/mapper/BookMapper.java
+++ src/main/java/org/example/asbe/mapper/BookMapper.java
@@ -2,9 +2,10 @@ package org.example.asbe.mapper;
 import org.example.asbe.model.dto.BookDTO;
 import org.example.asbe.model.entity.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
-@Mapper(componentModel = "spring")
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface BookMapper extends EntityMapper<BookDTO, Book>{
 
 }
