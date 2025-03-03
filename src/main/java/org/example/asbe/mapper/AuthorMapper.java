37b80f48ba549e5993e13e701bbc4a68889c66c8
Thaothantien <ntthao0809@gmail.com>
2025-03-03 23:48:15 +0700

    fix category + author

---
+++ src/main/java/org/example/asbe/mapper/AuthorMapper.java
@@ -0,0 +1,11 @@
+package org.example.asbe.mapper;
+
+import org.example.asbe.model.dto.AuthorDto;
+import org.example.asbe.model.entity.Author;
+import org.mapstruct.*;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface AuthorMapper extends EntityMapper<AuthorDto, Author> {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Author partialUpdate(AuthorDto authorDto, @MappingTarget Author author);
+}
