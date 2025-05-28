000884bd136a6c6e7433eaad605f57809f51e39d
Dao Quoc Khanh <khanhdq1@vnpost.vn>
2025-05-28 15:54:51 +0700

    add log error

--- src/main/java/org/example/asbe/model/dto/UserDTO.java
+++ src/main/java/org/example/asbe/model/dto/UserDTO.java
@@ -1,18 +1,10 @@
 package org.example.asbe.model.dto;
 
 import lombok.Data;
-import java.time.LocalDateTime;
-import java.time.Instant;
 
 @Data
 public class UserDTO {
-    private Integer id;
+    private Long id;
     private String username;
     private String email;
-    private String fullName;
-    private String phone;
-    private String address;
-    private String role;
-    private LocalDateTime createdAt;
-    private Instant updatedAt;
 }
