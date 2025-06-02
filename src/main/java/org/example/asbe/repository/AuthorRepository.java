6125bde1b78af1acadeafe99c07939a6627aef54
Thaothantien <ntthao0809@gmail.com>
2025-06-02 09:31:43 +0700

    update filter category + author

--- src/main/java/org/example/asbe/repository/AuthorRepository.java
+++ src/main/java/org/example/asbe/repository/AuthorRepository.java
@@ -5,9 +5,13 @@ import jakarta.validation.constraints.Size;
 import org.example.asbe.model.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     boolean existsByName(@Size(max = 100) @NotNull String name);
 
     boolean existsByBiography(String biography);
+
+    List<Author> findByNameContainingIgnoreCaseOrderById(String biography);
 }
